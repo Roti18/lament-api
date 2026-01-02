e5f6aa725dcef6fd9044f11eef4f8f22c49bad4b
Roti18 <akuronif@gmail.com>
2026-01-02 20:53:04 +0700

    feat(user): Add protected route for retrieving current user profile

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
 import * as userController from '../controllers/user.controller'
+import { jwtAuth } from '../middlewares/jwt.middleware'
 
 const route = new Hono()
 
+route.use('/me', jwtAuth)
+route.get('/me', userController.getMe)
 route.get('/', userController.listUsers)
 route.get('/:id', userController.getUser)
 
