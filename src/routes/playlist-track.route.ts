649786ca26c76a20ff2a52290e520470e385e42d
Roti18 <akuronif@gmail.com>
2026-01-02 20:51:47 +0700

    feat(playlist-track): Add routes for adding/removing tracks to playlists

--- src/routes/playlist-track.route.ts
+++ src/routes/playlist-track.route.ts
@@ -1,8 +1,14 @@
 import { Hono } from 'hono'
 import * as playlistTrackController from '../controllers/playlist-track.controller'
 
+import { jwtAuth } from '../middlewares/jwt.middleware'
+
 const route = new Hono()
 
+route.use('*', jwtAuth)
+
 route.get('/', playlistTrackController.listPlaylistTracks)
+route.post('/', playlistTrackController.addTrackToPlaylist)
+route.delete('/:id', playlistTrackController.removeTrackFromPlaylist)
 
 export default route
