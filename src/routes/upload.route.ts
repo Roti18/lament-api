fceefc4861f91031a757448a2aad4ca28284ef01
Roti18 <akuronif@gmail.com>
2026-01-02 20:52:26 +0700

    feat(upload): Secure upload route with JWT authentication

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono'
 import * as uploadController from '../controllers/upload.controller'
 
+import { jwtAuth } from '../middlewares/jwt.middleware'
+
 const route = new Hono()
+
+route.use('/', jwtAuth)
 
 route.post('/', uploadController.uploadFile)
 
